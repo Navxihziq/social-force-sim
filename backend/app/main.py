ed662deaddf27fadc9ca3a56ad6378bc8731cb6d
Navxihziq <zhixuanqi@outlook.com>
2025-01-05 17:44:23 -0500

    fix: backend port restriction updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,8 +39,7 @@ app.mount("/assets", StaticFiles(directory="./app/dist/assets"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
